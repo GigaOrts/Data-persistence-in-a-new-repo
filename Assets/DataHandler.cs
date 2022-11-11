d781e6857fe4e4150408effe701d607e43533f2f
GigaOrts <ortsortskhoev95@gmail.com>
2022-11-11 17:40:48 +0300

    Update DataHandler.cs

--- Assets/DataHandler.cs
+++ Assets/DataHandler.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class DataHandler : MonoBehaviour
 {
     public static DataHandler Instance;
+
+    public TextMesh HighScore;
 
     private void Awake()
     {
@@ -16,5 +19,23 @@ public class DataHandler : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public TextMesh HighScore;
+    }
+
+    public void SaveHighScore()
+    {
+        SaveData data = new SaveData();
+        data.HighScore = HighScore;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savedata.json", json);
     }
 }
